133dc4795cd61a5a842039314bdac41f9bb4606b
tracer-ics2017 <tracer@njuics.org>
2023-03-01 09:55:47 +0800

    >  compile 2013210 leo Linux leo-UBuntu22 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:55:47 up  7:19,  1 user,  load average: 0.49, 0.26, 0.21 2235f0da87b25e890c357b21db0fa28f97dc51c

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,12 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+//单步执行
+static int cmd_si(char *args){
+  printf("helloworld");
+}
+
+
 static struct {
   char *name;
   char *description;
@@ -46,6 +52,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si/s", "Step x (default 1)",cmd_si}
 
   /* TODO: Add more commands */
 
