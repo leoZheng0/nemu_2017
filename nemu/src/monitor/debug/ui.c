0ac846be7c30396355806808edbb3c9f90203cbc
tracer-ics2017 <tracer@njuics.org>
2023-03-10 23:55:16 +0800

    >  compile 2013210 leo Linux leo-UBuntu22 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:55:16 up  3:49,  1 user,  load average: 0.27, 0.27, 0.26 d0c0f3bfbeb45d7627ed5718cfa10ab32077dec2

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -94,9 +94,12 @@ static int cmd_x(char *args){
   TEST_VALID(arg==NULL);
   int N = atoi(arg);
 
-  arg = strtok(NULL, " ");
-  TEST_VALID(arg==NULL);
-  char *expr_str = arg;
+  char* expr_str = "";
+  while( (arg = strtok(NULL, " "))!=NULL){
+    strcat(expr_str,arg);
+  }
+  strcat(expr_str,"\0");
+  TEST_VALID(expr_str==NULL);
 
   //这里expr应该返回一个uint32类型的数值,因为地址一定为正
   bool success;
